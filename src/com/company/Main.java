bba81ad3cf7dbd647d178410e6df6595857166d2
valarfess <valarfess@gmail.com>
2021-01-28 20:44:15 +0300

    Домашка4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,13 +5,18 @@ public class Main {
     public static void main(String[] args) {
         Rectangle rectangle1 = new Rectangle(2,4);
         Rectangle rectangle2 = new Rectangle(4);
-        Rectangle rectangle3 = new Rectangle(3,5);
+        Rectangle rectangle3 = new Rectangle(2,4);
+
         rectangle1.printArea();
         rectangle2.printArea();
         rectangle3.printArea();
         rectangle1.printRectangleKind();
         rectangle2.printRectangleKind();
         rectangle3.printRectangleKind();
+
+        System.out.println(rectangle1.isTheSameRectangle(rectangle2));
+        System.out.println(rectangle2.isTheSameRectangle(rectangle3));
+        System.out.println(rectangle3.isTheSameRectangle(rectangle1));
 
 	// write your code here
     }
--- src/com/company/Rectangle.java
+++ src/com/company/Rectangle.java
@@ -1,43 +1,67 @@
 package com.company;
 
-public class Rectangle{
-    public double x,y;
+import java.util.Objects;
+
+public class Rectangle {
+    public double x, y;
+    private static int createdRectanglesCount;
+    private final static String RUSSIAN_CLASS_NAME = "Прямоугольник";
+    private final static String ENGLISH_CLASS_NAME = "Rectangle";
+
 
     //конструктор 1
-    public Rectangle(double x, double y){
+    public Rectangle(double x, double y) {
         this.x = x;
         this.y = y;
+        createdRectanglesCount++;
     }
 
     //конструктор 2
-    public Rectangle(double x){
+    public Rectangle(double x) {
         this.x = x;
         this.y = x;
+        createdRectanglesCount++;
     }
 
+    static void printRectanglesCount() {
+        System.out.println("Всего было создано " + Rectangle.createdRectanglesCount + " прямоугольников");
+    }
+
+    static boolean printClassName(boolean printInRussian) {
+        if (printInRussian) {
+            System.out.println(RUSSIAN_CLASS_NAME);
+        } else
+            System.out.println(ENGLISH_CLASS_NAME);
+    }
+
+
+
     //расчет площади
-    public double calculateArea(){
-        double calculateArea = x*y;
+    public double calculateArea() {
+        double calculateArea = x * y;
         return calculateArea;
 
     }
 
     //вывод площади в консоль
-    public void printArea(){
+    public void printArea() {
         System.out.println("Площадь прямоугольник - " + calculateArea());
     }
 
-//    //вывод в консоль типа прямоугольник/квадрат
-    public void printRectangleKind(){
-        if (x==y)
+    //    //вывод в консоль типа прямоугольник/квадрат
+    public void printRectangleKind() {
+        if (x == y)
             System.out.println("Это квадрат");
         else
             System.out.println("Это прямоугольник");
     }
 
+
     //вывод в консоль
-//    public boolean isTheSameRectangle(){
-//
-//    }
+    public boolean isTheSameRectangle(Rectangle rectangle) {
+        if (this.x == rectangle.x && this.y == rectangle.y) return true;
+        else return false;
+    }
+
 
 }
